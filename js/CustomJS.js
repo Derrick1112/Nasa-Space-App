ab8915a7dfbeee35451d5e7d075842f8c0c4fa0f
Alvin Sim <alvinsim74@gmail.com>
2021-09-30 23:43:49 +0800

    fixed pop up not working on js

--- js/CustomJS.js
+++ js/CustomJS.js
@@ -8,7 +8,11 @@ function filterTweetsProbe2 (){
     $('.block-update-card').filter('#ParkerRevolution').show();
 }
 
+
+    /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
 function myFunction() {
-  	var popup = document.getElementById("card-parker");
-  	popup.classList.toggle("show");
+    var popup1 = document.getElementById("card-parker")
+    var popup2 =  document.getElementById("card-parker-2")
+    popup1.classList.toggle("show");
+    popup2.classList.toggle("show");
 }
