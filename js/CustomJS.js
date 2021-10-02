4a1a7bcf9c8d7b5bd02e04b1f0a32e82eff856e3
Derrick1112 <derrick1112teo@gmail.com>
2021-10-02 17:07:35 +0800

    OSIRIS feature

--- js/CustomJS.js
+++ js/CustomJS.js
@@ -29,7 +29,7 @@ function myFunction() {
     probeUpdates.textContent = "Latest Updates from Parker Solar Probe";
     probeImages.src = "https://www.nasa.gov/sites/default/files/thumbnails/image/01_wispr-crop.png";
 
-    document.getElementById("probe-img").src = "img/p-parker.png"; 
+    //document.getElementById("probe-img").src = "../img/p-parker.png"; 
 }
 
 function ChangeToStereoInfo() {
@@ -50,5 +50,28 @@ function ChangeToStereoInfo() {
     probeUpdates.textContent = "Updates from Stereo Solar Probe";
     probeImages.src = "https://www.nasa.gov/sites/default/files/thumbnails/image/20150715_062759_n7eua_171.jpg";
 
-    document.getElementById("probe-img").src = "img/p-stereo.png"; 
+    document.getElementById("probe-img").src = "../img/p-stereo.png"; 
+}
+
+function ChangeToOsirisInfo() {
+    var popup1 = document.getElementById("card-parker");
+    var popup2 =  document.getElementById("card-parker-2");
+    if(!popup1.classList.contains("show")){
+        popup1.classList.toggle("show");
+        popup2.classList.toggle("show");
+    }
+    var probeTitle = document.getElementById("probe-title");
+    var probetext = document.getElementById("probe-text");
+    var probeUpdates = document.getElementById("Updates-Probe");
+    var probeImages = document.getElementById("Updates-img-Probe");
+	//var probeImg = document.getElementById("probe-img");
+
+	//probeImg.src = "../img/p-osiris.png";
+    probeTitle.textContent = "What is the OSIRIS-REx?";
+    probetext.textContent="Launched on Sept. 8, 2016, the Origins, Spectral Interpretation, Resource Identification, Security-Regolith Explorer (OSIRIS-REx) spacecraft traveled to a near-Earth asteroid called Bennu (formerly 1999 RQ36), and collected a sample of the of rocks and material from the surface that it will return to Earth in 2023. The mission will help scientists investigate how planets formed and how life began, as well as improve our understanding of asteroids that could impact Earth.";
+
+    probeUpdates.textContent = "Updates from OSIRIS-REx";
+    probeImages.src = "https://www.nasa.gov/sites/default/files/thumbnails/image/twelve-image_polycam_mosaic_12-2-18.png";
+
+    document.getElementById("probe-img").src = "../img/p-osiris.png"; 
 }
